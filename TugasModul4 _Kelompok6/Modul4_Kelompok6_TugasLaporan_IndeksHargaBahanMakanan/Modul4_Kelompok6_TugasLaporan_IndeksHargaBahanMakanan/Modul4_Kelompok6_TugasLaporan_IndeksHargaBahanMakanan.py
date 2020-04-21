24524d959552d9f9740f6d9b15aeb187fdb58ac2
SaifudinNA <apin99lightning@gmail.com>
2020-04-21 11:16:03 +0700

    Edit yang utama

--- TugasModul4 _Kelompok6/Modul4_Kelompok6_TugasLaporan_IndeksHargaBahanMakanan/Modul4_Kelompok6_TugasLaporan_IndeksHargaBahanMakanan/Modul4_Kelompok6_TugasLaporan_IndeksHargaBahanMakanan.py
+++ TugasModul4 _Kelompok6/Modul4_Kelompok6_TugasLaporan_IndeksHargaBahanMakanan/Modul4_Kelompok6_TugasLaporan_IndeksHargaBahanMakanan/Modul4_Kelompok6_TugasLaporan_IndeksHargaBahanMakanan.py
@@ -10,6 +10,16 @@ b = Carrefour.sayur("Buncis = Rp. 4,780/100gr", "Cabai = Rp. 12,590/100gr", "Kem
 c = Carrefour.hewani("Susu = Rp. 17,170/liter", "Telur = Rp. 22,900/kg", "Fillet Ayam = Rp. 4,250/100gr", "Cumi = Rp. 9,800/100gr")
 d = Carrefour.mie("Indomie Goreng = Rp. 2,500/pcs", "Indomie Rebus = Rp. 2,500/pcs", "Mie Sedaap Goreng = Rp. 2,800/pcs", "Mie Sedaap Rebus = Rp. 2,190/pcs")
 
+e = Hypermart.buah("Jeruk = Rp. 2,890/100gr", "Apel = Rp. 4,990/100gr", "Melon = Rp. 1,850/100gr", "Semangka Merah = Rp. 750/100gr")
+f = Hypermart.sayur("Buncis = Rp. 1,990/100gr", "Cabai = Rp. 3,890/100gr", "Kembang Kol = Rp. 3,590/100gr", "Jagung = Rp. 1,190/100gr")
+g = Hypermart.hewani("Susu = Rp. 26,950/liter", "Telur = Rp. 15,000/10pcs", "Fillet Ayam = Rp. 6,290/100gr", "Cumi = Rp. 7,890/100gr")
+h = Hypermart.mie("Indomie Goreng = Rp. 2,100/pcs", "Indomie Rebus = Rp. 2,167/pcs", "Mie Sedaap Goreng = Rp. 2,180/pcs", "Mie Sedaap Rebus = Rp. 2,200/pcs")
+
+i = Superindo.buah("Jeruk = Rp. 4,290/100gr", "Apel = Rp. 4,990/100gr", "Melon = Rp. 2,295/100gr", "Semangka Merah = Rp. 1,195/100gr")
+j = Superindo.sayur("Buncis = Rp. 2,695/100gr", "Cabai = Rp. 9,993/100gr", "Kembang Kol = Rp. 3,895/100gr", "Jagung = Rp. 4,833/pcs")
+k = Superindo.hewani("Susu = Rp. 16,322/liter", "Telur = Rp. 40,450/10pcs", "Fillet Ayam = Rp. 6,995/100gr", "Cumi = Rp. 9,495/100gr")
+l = Superindo.mie("Indomie Goreng = Rp. 2,190/pcs", "Indomie Rebus = Rp. 2,390/pcs", "Mie Sedaap Goreng = Rp. 2,500/pcs", "Mie Sedaap Rebus = Rp. 2,200/pcs")
+
 while j != "tidak" or j != "Tidak":
     print("Jenis Makanan Apa yang Ingin Anda Beli?")
     print("a. Buah-buahan\nb. Sayur-sayuran\nc. Produk Hewani\nd. Mie Instan")
@@ -21,9 +31,9 @@ while j != "tidak" or j != "Tidak":
         #harga di Carrefour
         a.hargabuah()
         #harga di Hypermart
-
+        e.hargabuah()
         #harga di Superindo
-
+        i.hargabuah()
         print("Apakah anda masih ingin berbelanja?")
         print("Ya.\nTidak.")
         j = input("Masukkan pilihan anda: ")
@@ -98,4 +108,3 @@ while j != "tidak" or j != "Tidak":
     else:
         print("Pilihan tidak tersedia.")
 
-    
--- TugasModul4 _Kelompok6/Modul4_Kelompok6_TugasLaporan_IndeksHargaBahanMakanan/Modul4_Kelompok6_TugasLaporan_IndeksHargaBahanMakanan/Superindo.py
+++ TugasModul4 _Kelompok6/Modul4_Kelompok6_TugasLaporan_IndeksHargaBahanMakanan/Modul4_Kelompok6_TugasLaporan_IndeksHargaBahanMakanan/Superindo.py
@@ -1,1 +1,39 @@
+class buah:
+    def __init__(self, jeruk, apel, melon, semangka):
+        self.jeruk = jeruk
+        self.apel = apel
+        self.melon = melon
+        self.semangka = semangka
 
+    def hargabuah(self):
+        print(f"Superindo:\n{self.jeruk}\n{self.apel}\n{self.melon}\n{self.semangka}\n")
+
+class sayur:
+    def __init__(self, buncis, cabai, kol, jagung):
+        self.buncis = buncis
+        self.cabai = cabai
+        self.kol = kol
+        self.jagung = jagung
+
+    def hargasayur(self):
+        print(f"Superindo:\n{self.buncis}\n{self.cabai}\n{self.kol}\n{self.jagung}\n")
+
+class hewani:
+    def __init__(self, susu, telur, ayam, cumi):
+        self.susu = susu
+        self.telur = telur
+        self.ayam = ayam
+        self.cumi = cumi
+
+    def hargahewani(self):
+        print(f"Superindo:\n{self.susu}\n{self.telur}\n{self.ayam}\n{self.cumi}\n")
+
+class mie:
+    def __init__(self, indomiegr, indomierbs, sedaapgr, sedaaprbs):
+        self.indomiegr = indomiegr
+        self.indomierbs = indomierbs
+        self.sedaapgr = sedaapgr
+        self.sedaaprbs = sedaaprbs
+
+    def hargamie(self):
+        print(f"Superindo:\n{self.indomiegr}\n{self.indomierbs}\n{self.sedaapgr}\n{self.sedaaprbs}\n")
